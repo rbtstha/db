18b221dd7987914aaa5e6bc2605c3fac6e23c32b
rbtstha <robitstha90@gmail.com>
2016-09-06 17:22:55 +0545

    welcome.php file is now edited

--- welcome.php
+++ welcome.php
@@ -1,11 +1,9 @@
 <html>
 <head></head><title>welcome</title>
 <body>
-	
+	<h1>hello</h1>
 <form method="POST" action="proc.php">
 Source<input type="text" name="source" placeholder="source adress"><br>
 Destination<input type="text" name="destination" palceholder="destination adress"><br>
 <input type="submit" name="sub">
-
-<?php
 
